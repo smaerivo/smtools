0267853ab4067703f0807b1aa6ea034cbe173ba3
Sven Maerivoet <sven.maerivoet@gmail.com>
2015-01-02 21:11:24 +0100

    Updated sources to 2015

--- SMTools/src/org/sm/smtools/swing/util/JUnfocusableTriggeredButton.java
+++ SMTools/src/org/sm/smtools/swing/util/JUnfocusableTriggeredButton.java
@@ -6,7 +6,7 @@
 // ------------------------------------------------
 
 /**
- * Copyright 2003-2014 Sven Maerivoet
+ * Copyright 2003-2015 Sven Maerivoet
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
